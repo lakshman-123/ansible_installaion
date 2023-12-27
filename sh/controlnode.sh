6cf72372b5e4bccd51e83a4396b80a5410952b99
lakshman <lr944944@gmail.com>
2023-12-27 02:32:34 +0100

    ansible cluster setup

--- sh/controlnode.sh
+++ sh/controlnode.sh
@@ -4,13 +4,13 @@ sudo apt install -y software-properties-common
 sudo apt install -y ansible
 sudo cp /vagrant/keys/demo.pem /home/vagrant/.ssh/.
 sudo chmod 600 /home/vagrant/.ssh/demo.pem
-sudo chown vagrant:vagrant demo.pem
+sudo chown vagrant:vagrant /home/vagrant/.ssh/demo.pem
 cat /vagrant/keys/demo.pub >> /home/vagrant/.ssh/authorized_keys
 sudo mkdir /etc/ansible
 sudo touch hosts
 sudo touch ansible.cfg
-sudo chown  vagrant:vagrant -R /etc/ansible
-cp /vagrant/hosts /etc/ansible/hosts
-cp /vagrant/ansible.cfg /etc/ansible/ansible.cfg
+sudo cp /vagrant/hosts /etc/ansible/hosts
+sudo cp /vagrant/ansible.cfg /etc/ansible/ansible.cfg
+sudo chown  vagrant:vagrant -R /etc/ansible/
 
 
